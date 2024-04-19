2de993fd5552a6b8cfb23cbc4d63ebf9f60cf7c2
Yogesh Kumar Sai <yogesh01041961@gmail.com>
2024-04-20 03:54:58 +0530

    update home page update dashboard login page update signup page update

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -5,6 +5,20 @@ import { Navigate, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import { server, AuthContext } from "../context/UserContext";
 
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
+
 const LoginPage = () => {
   const navigate = useNavigate();
   const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
@@ -24,9 +38,9 @@ const LoginPage = () => {
     });
   };
 
-  const handleTogglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
+  // const handleTogglePasswordVisibility = () => {
+  //   setShowPassword(!showPassword);
+  // };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -58,38 +72,77 @@ const LoginPage = () => {
 
   return (
     <>
-      <div className="login-container">
-        <div className="login-form-container">
-          <h2>Sign in</h2>
-          {!loading && (
-            <form onSubmit={handleSubmit}>
-              <input
-                type="email"
-                placeholder="Email"
-                name="email"
-                value={userDetail.email}
+    <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              value={userDetail.email}
                 onChange={handleChange}
-                required
-              />
-              <input
-                type={showPassword ? "text" : "password"}
-                placeholder="Password"
-                name="password"
-                value={userDetail.password}
+              autoFocus
+              size="small"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              value={userDetail.password}
                 onChange={handleChange}
-                required
-              />
-              <button type="submit">Login</button>
-            </form>
-          )}
-          {loading && <div className="loader"></div>}
-          <div className="signup-link">
-            <a href="/signup">Don't have an account? Sign Up</a>
-          </div>
-        </div>
-        <div className="login-image-container">
-        </div>
-      </div>
+              autoComplete="current-password"
+              size="small"
+
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
     </>
   );
 };
